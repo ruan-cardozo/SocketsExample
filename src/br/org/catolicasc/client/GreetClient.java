f77e84e671617259618848c550b3c11c51e7c772
ruanvcardozo@gmail.com <110867639+ruan-cardozo@users.noreply.github.com>
2023-06-06 21:36:52 -0300

    adicionar GreetClient.java

---
+++ src/br/org/catolicasc/client/GreetClient.java
@@ -0,0 +1,25 @@
+package br.org.catolicasc.client;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class GreetClient {
+	private Socket clientSocket;
+
+	private PrintWriter out;
+
+	private BufferedReader in;
+
+	public void start(String ip, int port){
+
+	}
+
+	public void stop(){
+
+	}
+
+	public String sendMessage(String msg){
+		return null;
+	}
+}
